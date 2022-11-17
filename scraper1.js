aa11cf3a4264b4d0e1bcb7a9de24a8fe65cce9fe
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-17 16:42:30 +1100

    Update scraper1.js

--- scraper1.js
+++ scraper1.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 async function scrapeItem(url) {
 	
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
 	const page = await browser.newPage();
 	await page.goto(url);
 
