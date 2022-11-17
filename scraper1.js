717ae983b1b764b106da9c7ddc24c905191e0f90
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-17 16:17:37 +1100

    Add files via upload

---
+++ scraper1.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+async function scrapeItem(url) {
+	
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url);
+
+	const [el] = await page.$x('/html/body/section[2]/div/div[1]/div[1]/dl/dd/span');
+	const text = await el.getProperty('p')
+	const textTXT = await text.jsonValue();
+
+	cnsole.log({textTXT});
+}
+
+scrapeItem('https://www.onlinejobs.ph/jobseekers/info/248015')
